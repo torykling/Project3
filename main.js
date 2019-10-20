9bae6faba97f2df10d4b99802774d725b720ece7
Tory Klingenstein <toryklingenstein@gmail.com>
2019-10-20 12:39:28 -0400

    add search

--- main.js
+++ main.js
@@ -1,18 +1,63 @@
  // api key c38e7bc912154c2fbac55e52653a38fc
 
- const url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=c38e7bc912154c2fbac55e52653a38fc"
+const url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=c38e7bc912154c2fbac55e52653a38fc"
+let section = document.querySelector("section");
+// this builds the search feature
+let input = document.querySelector(".input");
+let form = document.querySelector("form");
 
-let main = document.querySelector("main");
+form.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+    	console.log(input.value);
+    	fetch('https://newsapi.org/v2/everything?' +
+         	`q=${input.value}&` +
+          	'from=2019-10-20&' +
+          	'sortBy=popularity&' +
+          	'apiKey=c38e7bc912154c2fbac55e52653a38fc')
+    		.then(res => res.json())
+    		.then(res => {
+    			console.log("success2", res)
+    			for(let i = 0; i < 10; i++) {
+    				if (res.articles[i].content) {
+    					let button2 = section.appendChild(document.createElement("button"))
+    					button2.innerText = res.articles[i].title
+    					let description2 = section.appendChild(document.createElement("a"))
+    					description2.innerText = res.articles[i].content
+    					description2.classList.add("hidden")
+    					description2.classList.add("text")
+    					description2.setAttribute("href", res.articles[i].url)
+    					description2.setAttribute("target", "_blank")
+    				}
+    			}
+    			let buttons = document.querySelectorAll("button")
+    			let descriptions = document.querySelectorAll("a.text")
+    			for (let i = 0 ; i < buttons.length; i++) {
+					buttons[i].addEventListener("click", function(e) {
+						e.preventDefault();
+						descriptions[i].classList.toggle("hidden");
+		
+						for (let j = 0; j < buttons.length; j++) {
+							if (j != i) {
+							descriptions[j].classList.add("hidden");
+							}
+						} 
+					})
+				}
+    		})
+    		.catch(err => console.log("uh oh 2", err))
+})
 
+
+// This builds the main section with the default top stories
  fetch(url) 
  	.then(res => res.json())
  	.then(res => {
  		console.log("Success!", res.articles)
  		for (let i = 0; i < res.articles.length; i++) {
  			if (res.articles[i].content) {
- 				let button = main.appendChild(document.createElement("button"))
- 				let description = main.appendChild(document.createElement("a"))
- 				let author = main.appendChild(document.createElement("p"))
+ 				let button = section.appendChild(document.createElement("button"))
+ 				let description = section.appendChild(document.createElement("a"))
+ 				let author = section.appendChild(document.createElement("p"))
  				button.innerText = res.articles[i].title
  				description.innerText = res.articles[i].content
  				description.setAttribute("href", res.articles[i].url)
